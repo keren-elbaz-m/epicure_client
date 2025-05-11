1d52fc3f2f5ec75e2c7eee3407c30c32e0b56b07
keren-elbaz-m <kerene@moveo.co.il>
2025-05-11 13:26:01 +0300

    footer
    Create footer component for mobile and desktop and used text resources instead of hardcoded strings for footer links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/components/Navbar/Navbar";
-import Footer from "./components/Footer";
+import Footer from "@/app/components/footer/Footer";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
