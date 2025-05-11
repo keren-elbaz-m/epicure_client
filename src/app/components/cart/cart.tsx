378ad3374fe67bf33e26aba8777f2796e7774bc6
keren-elbaz-m <kerene@moveo.co.il>
2025-05-11 10:44:25 +0300

    Hook update
    updated cart component to use screenTupe hook instead of isDesktop

--- src/app/components/cart/cart.tsx
+++ src/app/components/cart/cart.tsx
@@ -4,9 +4,13 @@ import Image from "next/image";
 import empty_bag_icon from "@/app/assets/icons/cart_big_icon.svg";
 import styles from "@/app/components/cart/cart.module.scss";
 import useIsDesktop from "@/app/hooks/useIsDesktop";
+import { useScreenType } from "@/app/hooks/useScreenType";
+import { screenType } from "@/app/types";
 
 export default function Cart() {
-  const isDesktop = useIsDesktop();
+  // const isDesktop = useIsDesktop();
+  const screen = useScreenType();
+  const isDesktop=screen===screenType.DESKTOP;
 
   return (
     <div
