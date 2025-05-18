04bf5bf0d3e458fb3335537e1129b410f3a34de6
keren-elbaz-m <kerene@moveo.co.il>
2025-05-18 09:05:50 +0300

    applied global SCSS variables and mixins across components
    Integrated shared variables and mixins into most component SCSS files and removed hardcoded style.
    Remaining adjustments needed in search.module.scss and Card.module.scss.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,9 @@
 'use client'
 
 import { useScreenType } from "@/hooks/useScreenType";
-// import NavbarDesktop from "@/app/components/navbar/components/Navbar/NavbarDesktop";
 import NavbarDesktop from "@/components/Navbar/NavbarDesktop";
-
-// import NavbarMobile from "@/app/components/navbar/components/Navbar/NavbarMobile";
 import NavbarMobile from "@/components/Navbar/NavbarMobile";
 import { screenType } from "@/types";
-
 
 export default function Navbar() {
     const currentScreenType = useScreenType();
