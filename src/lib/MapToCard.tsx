1062935dfc1c4e86a4f8e3515253f88ba073050a
keren-elbaz-m <kerene@moveo.co.il>
2025-05-21 14:03:42 +0300

    Update MapToCard.tsx
    Moved renderContent function inside MapToCards

--- src/lib/MapToCard.tsx
+++ src/lib/MapToCard.tsx
@@ -13,48 +13,48 @@ interface CardItem {
   imageUrl: string;
 }
 
-function renderContent(item: unknown, type:SectionPart): React.ReactNode {
-  switch (type) {
-    case SectionPart.RESTAURANT:
-      const restaurant = item as Restaurant;
-      return (
-        <>
-          <p>{restaurant.chefName}</p>
-          <RatingStars rating={restaurant.rating} max={5} />
-        </>
-      );
-
-    case SectionPart.DISH:
-      const dish = item as Dish;
-      return (
-        <>
-          <div>
-            <Image
-              src={dish.type.iconUrl}
-              alt={dish.type.name}
-              width={30}
-              height={30}
-            />
-          </div>
-          <p>{dish.ingredients.join(', ')}</p>
-          <p>
-            <span>₪</span>{dish.price}
-          </p>
-        </>
-      );
-
-    default:
-      return null;
-  }
-}
-
-
 
 export function MapToCards(
   data: unknown[] | undefined | null,
   type: SectionPart
 ): ReactElement[] {
   if (!data) return [];
+
+    function renderContent(item: unknown): React.ReactNode {
+    switch (type) {
+        case SectionPart.RESTAURANT:
+        const restaurant = item as Restaurant;
+        return (
+            <>
+            <p>{restaurant.chefName}</p>
+            <RatingStars rating={restaurant.rating} max={5} />
+            </>
+        );
+
+        case SectionPart.DISH:
+        const dish = item as Dish;
+        return (
+            <>
+            <div>
+                <Image
+                src={dish.type.iconUrl}
+                alt={dish.type.name}
+                width={40}
+                height={40}
+                />
+            </div>
+            <p>{dish.ingredients.join(', ')}</p>
+            <p>
+                <span>₪</span>{dish.price}
+            </p>
+            </>
+        );
+
+        default:
+        return null;
+        }
+    }
+
 
   return data.map((item) => {
     if (!item || typeof item !== 'object') return null;
@@ -65,7 +65,7 @@ export function MapToCards(
     return (
       <Card key={baseItem.id} item={baseItem} variant={type}>
         {
-          renderContent(item, type)
+          renderContent(item)
         }
       </Card>
     );
