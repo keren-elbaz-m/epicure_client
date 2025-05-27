b2a26c71bd1023df00df42b59c32ba3866a6ea01
keren-elbaz-m <kerene@moveo.co.il>
2025-05-27 16:56:12 +0300

    navbar highlight active menu
    highlight active menu item based on pathname

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,10 +4,12 @@ import { useScreenType } from "@/hooks/useScreenType";
 import NavbarDesktop from "@/components/Navbar/NavbarDesktop";
 import NavbarMobile from "@/components/Navbar/NavbarMobile";
 import { screenType } from "@/types";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
     const currentScreenType = useScreenType();
+    const pathname = usePathname();
 
-    return currentScreenType === screenType.DESKTOP ? <NavbarDesktop /> : <NavbarMobile />;
+    return currentScreenType === screenType.DESKTOP ? <NavbarDesktop pathname={pathname}/> : <NavbarMobile />;
 
 }
