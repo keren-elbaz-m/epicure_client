c52c4882b89e797f6311289b6f0463c7835cd8e5
keren-elbaz-m <kerene@moveo.co.il>
2025-06-01 17:52:09 +0300

    Fixed navbar style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+import style from "@/app/Layout.module.scss";
 
 
 const geistSans = Geist({
@@ -28,8 +29,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        
         <Navbar/>
-          <main>{children}</main>
+          <main >{children}</main>
         <Footer/>
       </body>
     </html>
