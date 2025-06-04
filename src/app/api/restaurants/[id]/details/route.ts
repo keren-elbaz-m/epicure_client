cd0b09615691485f4cee0687cca7b3afc05f98ee
keren-elbaz-m <kerene@moveo.co.il>
2025-06-04 10:39:46 +0300

    Update route.ts
    wrapped fetching logic in try and catch for better error handling.

--- src/app/api/restaurants/[id]/details/route.ts
+++ src/app/api/restaurants/[id]/details/route.ts
@@ -3,6 +3,9 @@ import { restaurants } from "@/data/restaurants.data";
 import { dishes } from "@/data/dishes.data";
 
 export async function GET(request: Request, context: { params: { id: string } }) {
+  try{
+
+  
     const { id } = context.params;
     const restaurantId = Number(id);
     const restaurant = restaurants.find(r => r.id === restaurantId);
@@ -17,4 +20,8 @@ export async function GET(request: Request, context: { params: { id: string } })
     restaurant,
     dishes: restaurantDishes,
   });
+  }catch(error){
+    console.error("Unexpected error in GET /api/restaurant/[id]/details:", error);
+    return NextResponse.json({error: "Internal server error"}, {status:500});
+  }
 }
