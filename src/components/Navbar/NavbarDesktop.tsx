d8ac9c9728b649d00e73a1f70802975c4e6411ea
keren-elbaz-m <kerene@moveo.co.il>
2025-06-18 16:03:49 +0300

    navigation for restaurant's dishes
    Wrapped restaurant wide cards with Link to enable navigation to restaurant page

--- src/components/Navbar/NavbarDesktop.tsx
+++ src/components/Navbar/NavbarDesktop.tsx
@@ -1,4 +1,4 @@
-import styles from "@/components/Navbar/Navbar.module.scss"
+import styles from "@/components/Navbar/Navbar.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -16,28 +16,29 @@ import { RESOURES } from "@/data/text";
 import { getPageContext } from "@/lib/utils/getPageContext";
 
 type NavbarDesktopProps = {
-  pathname: string;
-}
-export default function NavbarDesktop({pathname}:NavbarDesktopProps) {
+    pathname: string;
+};
+export default function NavbarDesktop({ pathname }: NavbarDesktopProps) {
+    const [isCartOpen, setIsCartOpen] = useState(false);
+    const toggleCart = () => setIsCartOpen((prev) => !prev);
+    const closeCart = () => setIsCartOpen(false);
+    const { isRestaurants, isChefs } = getPageContext(pathname);
 
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const toggleCart = () => setIsCartOpen(prev => !prev);
-  const closeCart = () => setIsCartOpen(false);
-  const {isRestaurants, isChefs} = getPageContext(pathname);
+    return (
+        <div className={layoutStyle.navbarWrapper}>
+            <nav className={styles.navbar}>
+                <div className={styles.desktopContent}>
+                    <div className={styles.leftSection}>
+                        <Link href="/">
+                            <div className={styles.logo}>
+                                <Image
+                                    src={epicure_logo_icon}
+                                    alt="epicure logo icon"
+                                />
+                            </div>
+                        </Link>
 
-  return (
-    <div className={layoutStyle.navbarWrapper}>
-    <nav className={styles.navbar}>
-      <div className={styles.desktopContent}>
-        <div className={styles.leftSection}>
-
-          <Link href="/">
-            <div className={styles.logo}>
-              <Image src={epicure_logo_icon} alt="epicure logo icon" />
-            </div>
-          </Link>
-
-<!--           <div className={styles.logoIcon}>
+                        {/* <div className={styles.logoIcon}>
             <Link href="/">
               <div className={styles.logo}>
                 <Image src={epicure_logo_icon} alt="epicure logo icon" />
@@ -47,47 +48,65 @@ export default function NavbarDesktop({pathname}:NavbarDesktopProps) {
               {RESOURES.app.appName.toUpperCase()}
             </span>
 
-          </div> -->
+          </div>  */}
 
-          
+                        <div className={styles.links}>
+                            <Link
+                                href="/restaurants"
+                                className={`${styles.navLink} ${
+                                    isRestaurants ? styles.active : ""
+                                }`}
+                            >
+                                Restaurants
+                            </Link>
+                            <Link
+                                href="/chefs"
+                                className={`${styles.navLink} ${
+                                    isChefs ? styles.active : ""
+                                }`}
+                            >
+                                Chefs
+                            </Link>
+                        </div>
+                    </div>
 
-          <div className={styles.links}>
-            <Link
-              href="/restaurants"
-              className={`${styles.navLink} ${isRestaurants ? styles.active : ''}`}
+                    <div className={styles.icons}>
+                        <button className={styles.buttonIconsStyle}>
+                            <Image
+                                src={search_icon}
+                                alt="search icon"
+                                width={24}
+                                height={24}
+                            />
+                        </button>
+                        <Image
+                            src={person_icon}
+                            alt="person icon"
+                            width={24}
+                            height={24}
+                        />
+
+                        <button
+                            className={styles.buttonIconsStyle}
+                            onClick={toggleCart}
+                        >
+                            <Image
+                                src={shopping_bag_icon}
+                                alt="shopping bag icon"
+                                width={24}
+                                height={24}
+                            />
+                        </button>
+                    </div>
+                </div>
+            </nav>
+            <DropDown
+                isOpen={isCartOpen}
+                onClose={closeCart}
+                type={DropDownType.CART}
             >
-              Restaurants
-            </Link>    
-            <Link
-              href="/chefs"
-              className={`${styles.navLink} ${isChefs ? styles.active : ''}`}
-            >
-              Chefs
-            </Link>          
-          </div>
+                <Cart />
+            </DropDown>
         </div>
-
-        <div className={styles.icons}>
-            <button className={styles.buttonIconsStyle}>
-                <Image src={search_icon} alt="search icon" width={24} height={24}/>
-            </button>
-                <Image src={person_icon} alt="person icon" width={24} height={24}/>
-
-            <button className={styles.buttonIconsStyle} onClick={toggleCart}>
-                <Image src={shopping_bag_icon} alt="shopping bag icon" width={24} height={24}/>
-            </button>
-
-        </div>
-
-      </div>
-    </nav>
-    <DropDown
-      isOpen={isCartOpen}
-      onClose={closeCart}
-      type={DropDownType.CART}
-    >
-      <Cart />
-    </DropDown>
-  </div>
-  );
+    );
 }
