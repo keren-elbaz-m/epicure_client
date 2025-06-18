9b8711da977cfe8f59a5f0edfa026fcf4aa3e52b
keren-elbaz-m <kerene@moveo.co.il>
2025-06-18 14:17:39 +0300

    Update Api.constans.ts

--- src/constans/Api.constans.ts
+++ src/constans/Api.constans.ts
@@ -1,8 +1,10 @@
 export const API_ROUTES = {
-    BASE_URL: 'http://localhost:3000/api',
-    RESTAURANTS: '/restaurants',
-    DISHES: '/dish',
-    CHEFS: '/chef',
-}
+    BASE_URL: "http://localhost:3500/api",
+    RESTAURANTS: "/restaurants",
+    DISHES: "/dish",
+    CHEFS: "/chef",
+};
 
-export const API_CHEF_BY_ID = (id: number | string) => `/chef/${id}`; 
+export const API_CHEF_BY_ID = (id: number | string) => `/chef/${id}`;
+
+export const BASE_API_URL = "http://localhost:3500/api";
