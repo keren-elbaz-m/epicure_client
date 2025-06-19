21bc2892326444a17862dffd40ce0554494c6b5b
keren-elbaz-m <kerene@moveo.co.il>
2025-06-19 13:32:49 +0300

    improved visual
    improved visual

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,30 +1,47 @@
-
 import Image from "next/image";
-import style from '@/components/About/About.module.scss';
+import style from "@/components/About/About.module.scss";
+import layoutStyle from "@/./app/Layout.module.scss";
 import epicureLogo from "@/../public/images/about/epicure-full-logo.png";
-import {RESOURES} from "@/data/text";
-import { contactIcons } from "@/constans/About.constans"; 
+import { RESOURES } from "@/data/text";
+import { contactIcons } from "@/constans/About.constans";
 
-export default function About(){
-  return (
-    <div className={style.container}>
-        <div className={style.secondaryContainer}>
-            <div className={style.textBlock}>
-                <h1 className={style.title}>{RESOURES.homepage.aboutUs}</h1>
-                <p className={style.description}>{RESOURES.homepage.description}</p>
-            </div>
-            <div className={style.iconsWrapper}>
-                {contactIcons.map((icon, index) => (
-                    <button key={index} className={style.iconBlock}>
-                        <Image src={icon.src} alt={`${icon.label} icon`} className={style.iconImage}/>
-                        <p>{icon.label}</p>
-                    </button>
-                ))}
+export default function About() {
+    return (
+        <div className={style.contWrap}>
+            <div className={style.layout}>
+                <div className={style.container}>
+                    <div className={style.secondaryContainer}>
+                        <div className={style.textBlock}>
+                            <h1 className={style.title}>
+                                {RESOURES.homepage.aboutUs}
+                            </h1>
+                            <p className={style.description}>
+                                {RESOURES.homepage.description}
+                            </p>
+                        </div>
+                        <div className={style.iconsWrapper}>
+                            {contactIcons.map((icon, index) => (
+                                <button key={index} className={style.iconBlock}>
+                                    <Image
+                                        src={icon.src}
+                                        alt={`${icon.label} icon`}
+                                        className={style.iconImage}
+                                    />
+                                    <p>{icon.label}</p>
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+                    <div className={style.logo}>
+                        <Image
+                            src={epicureLogo}
+                            alt="Epicure logo"
+                            width={120}
+                            height={120}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
-        <div className={style.logo}>
-            <Image src={epicureLogo} alt="Epicure logo" width={120} height={120} />
-        </div>
-    </div>
-  );
-} 
+    );
+}
